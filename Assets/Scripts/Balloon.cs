1c3679f36baaa94d623d2ab9c47ea6c48e18ec12
PeacefulKim <127909764+PeacefulKim@users.noreply.github.com>
2025-09-23 17:47:22 +0900

    feat ba

---
+++ Assets/Scripts/Balloon.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Balloon : MonoBehaviour
+{
+    public GameObject balloonPrefab;
+    public GameObject successUI;
+    public float spawnTime = 2f;
+    public float moveSpeed = 2f;
+    
+    void Start()
+    {
+        InvokeRepeating("SpawnBalloon", 0f, spawnTime);
+    }
+    void SpawnBalloon()
+    {
+        if (successUI == null) return;
+
+        float randomX;
+        if (Random.value < 0.5f) randomX = Random.Range(-9f, -5f);
+        else randomX = Random.Range(9f, 5f);
+
+        Vector3 spawnPos = new Vector3(randomX, -6f, 0f);
+
+        GameObject balloon = Instantiate(balloonPrefab, spawnPos, Quaternion.identity);
+        balloon.AddComponent<BalloonMover>().moveSpeed = moveSpeed;
+        Destroy(balloon, 6f);
+    }
+}
+
+public class BalloonMover : MonoBehaviour
+{
+    public float moveSpeed = 2f;
+    void Update()
+    {
+        transform.Translate(Vector3.up * moveSpeed * Time.deltaTime);
+    }
+}
