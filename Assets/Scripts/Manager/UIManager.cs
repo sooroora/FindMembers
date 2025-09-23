7317effe58217f6af396d4caabc48e0e536bb856
yun <ycy6797@naver.com>
2025-09-23 21:13:00 +0900

    받아오기

--- Assets/Scripts/Manager/UIManager.cs
+++ Assets/Scripts/Manager/UIManager.cs
@@ -1,5 +1,8 @@
 ﻿using System.Collections.Generic;
 using UnityEngine;
+using System.Collections;
+using System.Reflection;
+using UnityEngine.UI;
 
 public class UIManager : MonoBehaviour
 {
@@ -42,4 +45,37 @@ public class UIManager : MonoBehaviour
                 uiStack.Peek().SetActive(true);
         }
     }
+
+    [SerializeField] Slider masterSlider;
+    [SerializeField] Slider fxSlider;
+    [SerializeField] Slider bgmSlider;
+    void Start()
+    {
+
+        masterSlider.onValueChanged.AddListener(SetMasterVolume);
+        fxSlider.onValueChanged.AddListener(SetFxVolume);
+        bgmSlider.onValueChanged.AddListener(SetBgmVolume);
+
+        masterSlider.value = 0.5f;
+        fxSlider.value = 0.5f;
+        bgmSlider.value = 0.5f;
+    }
+
+    // 채윤님 AudioManager 나오면  설정하는 내용 추가 필요
+    public void SetMasterVolume(float volume)
+    {
+        AudioListener.volume = volume;
+    }
+
+    public void SetFxVolume(float volume)
+    {
+        AudioManager.Instance.SetSfxVolume(volume);
+    }
+
+    public void SetBgmVolume(float volume)
+    {
+        AudioManager.Instance.SetBgmVolume(volume);
+    }
+
+
 }
