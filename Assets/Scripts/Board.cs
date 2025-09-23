2d7965e7a116e38ce144ae34e007bb322e4ec537
jaewoo0530 <6672jjw@gmail.com>
2025-09-23 10:25:25 +0900

    Board 스크립트 변경

--- Assets/Scripts/Board.cs
+++ Assets/Scripts/Board.cs
@@ -6,29 +6,47 @@ using UnityEngine;
 public class Board : MonoBehaviour
 {
     public GameObject card;
-    public int pair = 8;
 
     void Start()
     {
-        int[] arr = new int[pair * 2];
-        for (int i = 0; i < pair; i++)
+        if(GameManager.Instance.currentLevel == 0)
         {
-            arr[i * 2] = i;
-            arr[i * 2 + 1] = i;
+            BoardSetting(8);
         }
-
-        arr = arr.OrderBy(x => Random.Range(0, 1000)).ToArray();
-
-        for (int i = 0; i < arr.Length; i++)
+        else if (GameManager.Instance.currentLevel == 1)
         {
-            float x = (i % 4) * 1.4f - 2.1f;
-            float y = (i / 4) * 1.4f - 3.0f;
-
-            GameObject obj = Instantiate(card, this.transform);
-            obj.transform.position = new Vector2(x, y);
-            obj.GetComponent<Card>().Setting(arr[i]);
+            BoardSetting(10);
         }
+        else if (GameManager.Instance.currentLevel == 2)
+        {
+            BoardSetting(15);
+        }
+    }
 
-        GameManager.Instance.cardCount = arr.Length;
+    private void BoardSetting(int pair)
+    {
+        if (GameManager.Instance.currentLevel == 0)
+        {
+            int[] arr = new int[pair * 2];
+            for (int i = 0; i < pair; i++)
+            {
+                arr[i * 2] = i;
+                arr[i * 2 + 1] = i;
+            }
+
+            arr = arr.OrderBy(x => Random.Range(0, 1000)).ToArray();
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                float x = (i % 4) * 1.4f - 2.1f;
+                float y = (i / 4) * 1.4f - 3.0f;
+
+                GameObject obj = Instantiate(card, this.transform);
+                obj.transform.position = new Vector2(x, y);
+                obj.GetComponent<Card>().Setting(arr[i]);
+            }
+
+            GameManager.Instance.cardCount = arr.Length;
+        }
     }
 }
