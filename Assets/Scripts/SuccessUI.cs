bc42de56d47fcf65519cec12e80168ac9855f42d
PeacefulKim <127909764+PeacefulKim@users.noreply.github.com>
2025-09-23 11:34:01 +0900

    ㅇㅁㅇ

--- Assets/Scripts/SuccessUI.cs
+++ Assets/Scripts/SuccessUI.cs
@@ -1,29 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class SuccessUI : MonoBehaviour
 {
-    public GameObject balloon;
+    int list = 0;
+    string[] arr = { "전수라", "양채윤", "황준영", "정재우", "박상현" };
+    public Text nameTxt;
 
     void Start()
     {
-        NextSpawn();
+
     }
 
     void Update()
     {
-        
+        nameTxt.text = arr[0];
     }
 
-    void SpawnBalloon()
-    {
-        Instantiate(balloon);
-    }
 
-    void NextSpawn()
-    {
-        float delay = Random.Range(3f, 10f);
-        Invoke("SpawnBalloon", delay);
-    }
 }
