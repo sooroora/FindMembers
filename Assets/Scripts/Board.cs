6c0b23046cec2cad26319a7c6428650bf2096408
jaewoo0530 <6672jjw@gmail.com>
2025-09-23 16:49:09 +0900

    Squashed commit of the following:
    commit e867e63ce5c0d6b72f029fa0a00c02fddb834ca1
    Author: jaewoo0530 <6672jjw@gmail.com>
    Date:   Tue Sep 23 16:46:55 2025 +0900
    
        Card 프리팹 수정
    
    commit a448c19e0dd899dc875171473a094d97f65abd05
    Author: jaewoo0530 <6672jjw@gmail.com>
    Date:   Tue Sep 23 16:46:16 2025 +0900
    
        Board, Card 스크립트 수정

--- Assets/Scripts/Board.cs
+++ Assets/Scripts/Board.cs
@@ -6,10 +6,11 @@ using UnityEngine;
 public class Board : MonoBehaviour
 {
     public GameObject card;
+    private List<GameObject> cardList = new List<GameObject>();
 
     void Start()
     {
-        if(GameManager.Instance.currentLevel == 0)
+        if (GameManager.Instance.currentLevel == 0)
         {
             BoardSetting(8);
         }
@@ -36,35 +37,44 @@ public class Board : MonoBehaviour
 
         if (GameManager.Instance.currentLevel == 0)
         {
-            StartCoroutine(DelayAnimation(arr, 4, new Vector2(1.3f, 1.3f)));
+            StartCoroutine(DelayAnimation(arr, 4, 1.4f, -2.1f, -3.0f, new Vector2(1.3f, 1.3f)));
         }
 
         if (GameManager.Instance.currentLevel == 1)
         {
-            StartCoroutine(DelayAnimation(arr, 4, new Vector2(1.1f, 1.1f)));
+            StartCoroutine(DelayAnimation(arr, 4, 1.4f, -2.1f, -4.2f, new Vector2(1.2f, 1.2f)));
         }
 
         if (GameManager.Instance.currentLevel == 2)
         {
-            StartCoroutine(DelayAnimation(arr, 5, new Vector2(1.0f, 1.0f)));
+            StartCoroutine(DelayAnimation(arr, 5, 1.1f, -2.2f, -4.2f, new Vector2(1.0f, 1.0f)));
         }
 
         GameManager.Instance.cardCount = arr.Length;
     }
 
-    IEnumerator DelayAnimation(int[] arr, int widthCount, Vector2 cardScale)
+    IEnumerator DelayAnimation(int[] arr, int widthCount, float cardSpacing, float xStart, float yStart, Vector2 cardScale)
     {
+        cardList.Clear();
+
         for (int i = 0; i < arr.Length; i++)
         {
-            float x = (i % widthCount) * 1.4f - 2.1f;
-            float y = (i / widthCount) * 1.4f - 3.0f;
+            float x = (i % widthCount) * cardSpacing + xStart;
+            float y = (i / widthCount) * cardSpacing + yStart;
 
             GameObject obj = Instantiate(card, transform);
             obj.transform.position = new Vector2(x, y);
             obj.transform.localScale = cardScale;
             obj.GetComponent<Card>().Setting(arr[i]);
 
+            cardList.Add(obj);
+
             yield return new WaitForSeconds(0.2f);
+        }
+
+        foreach (GameObject c in cardList)
+        {
+            c.GetComponent<Card>().ActivateCard();
         }
     }
 }
