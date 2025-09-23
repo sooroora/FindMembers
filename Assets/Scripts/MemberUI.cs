01dc49298c57a38a722ff6f5a1578e43bc47e2f6
PeacefulKim <127909764+PeacefulKim@users.noreply.github.com>
2025-09-23 16:19:59 +0900

    Origin/feat/success UI (#25)
    * feat memUI

--- Assets/Scripts/MemberUI.cs
+++ Assets/Scripts/MemberUI.cs
@@ -8,11 +8,18 @@ public class MemberUI : MonoBehaviour
     public GameObject listPanel;
     public GameObject memberPanel;
     public SuccessUI memberCtrl;
+    public GameObject bgPanel;
 
     public void OpenPanel(int memberIndex)
     {
         listPanel.SetActive(false);
         memberPanel.SetActive(true);
         memberCtrl.SetMember(memberIndex);
+    }
+
+    public void ClosePanel()
+    {
+        memberPanel.SetActive(false);
+        bgPanel.SetActive(false);
     }
 }
