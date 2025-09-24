2283f2695d146c842985fc0b5bab47af55123be7
yun <ycy6797@naver.com>
2025-09-24 11:02:26 +0900

    오디오 통일

--- Assets/Scripts/VolumeSlider.cs
+++ Assets/Scripts/VolumeSlider.cs
@@ -1,21 +1,23 @@
-using System.Collections.Generic;
-using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 using System.Reflection;
+using UnityEngine;
 using UnityEngine.UI;
+
 public class VolumeSlider : MonoBehaviour
 {
     [SerializeField] Slider masterSlider;
     [SerializeField] Slider fxSlider;
     [SerializeField] Slider bgmSlider;
+
+
     void Start()
     {
-
         masterSlider.onValueChanged.AddListener(SetMasterVolume);
         fxSlider.onValueChanged.AddListener(SetFxVolume);
         bgmSlider.onValueChanged.AddListener(SetBgmVolume);
 
-        masterSlider.value = 0.5f;
+        masterSlider.value = 1f;
         fxSlider.value = 0.5f;
         bgmSlider.value = 0.5f;
     }
