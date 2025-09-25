c813fcec4c7a541959979a18e9eebf51938c5605
yun <ycy6797@naver.com>
2025-09-25 10:18:07 +0900

    양채윤스크립트 주석

--- Assets/Scripts/Manager/AudioManager.cs
+++ Assets/Scripts/Manager/AudioManager.cs
@@ -6,67 +6,6 @@ public class AudioManager : MonoBehaviour
 {
     public static AudioManager Instance;
 
-    public AudioSource BGM;
-    public AudioSource CardFlip;
-    public AudioSource Matched;
-    public AudioSource ClookTicking;
-    public AudioSource Failed;
-    public AudioSource clear;
-    public AudioSource TimeOver;
-    public AudioSource[] BtnClick;
-
-    public void PlayOneShot(string sfxname)
-    {
-        switch (sfxname)
-        {
-            case "CardFlip":
-                CardFlip.PlayOneShot(CardFlip.clip);
-                break;
-
-            case "Matched":
-                Matched.PlayOneShot(Matched.clip);
-                break;
-            
-            case "ClookTicking":
-                Failed.PlayOneShot(ClookTicking.clip);
-                break;
-
-            case "Failed":
-                Failed.PlayOneShot(Failed.clip);
-                break;
-
-            case "clear":
-                Failed.PlayOneShot(clear.clip);
-                break;
-
-            case "TimeOver":
-                Failed.PlayOneShot(TimeOver.clip);
-                break;
-        }
-    }
-
-    public void PlayBGM()
-    {
-        BGM.loop = true;
-        BGM.Play();
-    }
-
-    public void PlayRandomClick()
-    {
-        int randomclick = Random.Range(0, 4);
-        BtnClick[randomclick].PlayOneShot(BtnClick[randomclick].clip);
-    }
-
-    public void UpdateBgmPitch()
-    {
-        BGM.pitch = 1.25f;
-    }
-
-    public void ResetBgmPitch()
-    {
-        BGM.pitch = 1f;
-    }
-
     private void Awake()
     {
         if (Instance == null)
@@ -76,36 +15,98 @@ public class AudioManager : MonoBehaviour
         }
         else
         {
-            Destroy(gameObject);
+            Destroy(gameObject);//오디오매니저 싱글톤 만들어서 씬 넘어가도 오디오매니저 파괴 안되게 만들기, 오디오매니저를 만들어서 다른 곳에서 효과음 나올 타이밍에 함수만 갖다쓰면 바로 호출되게 만들기
+        }
+    }
+    public AudioSource BGM;
+    public AudioSource CardFlip;
+    public AudioSource Matched;
+    public AudioSource ClookTicking;
+    public AudioSource Failed;
+    public AudioSource clear;
+    public AudioSource TimeOver;
+    public AudioSource[] BtnClick;//오디오 소스 선언, 클릭버튼 사운드가 4가지가 랜덤으로 들어가는 식이라 배열을 통해 선언함, 인스펙터창에서 4칸 추가해서 사운드 추가
+
+    public void PlayOneShot(string sfxname)//다른 스크립트에서 해당하는 사운드 이름을 문자열형식으로 넣어두면 바로 불러올 수 있게 함수를 작성함
+    {
+        switch (sfxname)
+        {
+            case "CardFlip":
+                CardFlip.PlayOneShot(CardFlip.clip);// 카드 뒤집기 효과음
+                break; //케이스 안에 있는 내용만 실행하고 종료
+
+            case "Matched":
+                Matched.PlayOneShot(Matched.clip);// 카드 짝 맞췄을 때 효과음
+                break;
+            
+            case "ClookTicking":
+                Failed.PlayOneShot(ClookTicking.clip);// 시계 초침 소리 
+                break;
+
+            case "Failed":
+                Failed.PlayOneShot(Failed.clip);// 실패 효과음
+                break;
+
+            case "clear":
+                Failed.PlayOneShot(clear.clip);// 클리어 효과음
+                break;
+
+            case "TimeOver":
+                Failed.PlayOneShot(TimeOver.clip);// 제한 시간 종료 효과음
+                break;
         }
     }
 
+    public void PlayBGM()//브금 틀어주는 함수
+    {
+        BGM.loop = true; // 배경음악을 반복 재생하도록 설정
+        BGM.Play();// 배경음악 재생 시작
+    }
+
+    public void PlayRandomClick()//클릭사운드 호출하는 함수
+    {
+        int randomclick = Random.Range(0, 4);//0부터 3의 랜덤한 범위 이내에서
+        BtnClick[randomclick].PlayOneShot(BtnClick[randomclick].clip);//선택된 버튼클릭에서 버튼 클릭 효과음을 1회 재생.
+    }
+
+    public void UpdateBgmPitch()
+    {
+        BGM.pitch = 1.25f;// 배경음악 재생 속도(피치)를 1.25배로 변경
+    }
+
+    public void ResetBgmPitch()
+    {
+        BGM.pitch = 1f;// 배경음악 재생 속도를 기본값(정상 속도)으로 초기화
+    }
+
+   
+
     private void Start()
     {
-        PlayBGM();
+        PlayBGM();// 게임 시작 시 자동으로 배경음악 재생
     }
 
     public void SetMasterVolume(float volume)
     {
-        AudioListener.volume = volume;  //전체소리조절
+        AudioListener.volume = volume;  //전체소리조절하는데 AudioListener 활용
     }
 
     public void SetBgmVolume(float volume)
     {
-        BGM.volume = volume;    //나머지 소리는 오디오소스에서 변경
+        BGM.volume = volume;    //나머지 소리는 오디오소스에서 변경 브금 볼륨
     }
 
-    public void SetSfxVolume(float volume)
+    public void SetSfxVolume(float volume)//효과음 볼륨크기를 조절하는 함수, 이 안에다가 효과음들 다 집어넣어서 통일시키기
     {
         CardFlip.volume = volume;
         Matched.volume = volume;
-        ClookTicking.volume = volume * 0.05f;
+        ClookTicking.volume = volume * 0.05f; //사운드가 얘만 커서 소리크기 임의조정
         Failed.volume = volume;
         clear.volume = volume;
         TimeOver.volume = volume;
-        foreach (AudioSource click in BtnClick)
+        foreach (AudioSource click in BtnClick)//BtnClick 안에 있는 오디오소스 클릭 사운드들을 모두 순서대로 반복 실행
         {
-            click.volume = volume;
+            click.volume = volume;//BtnClick 사운드 4가지 볼륨은 변수 volume값을 가짐
         }
     }
 }
--- Assets/Scripts/VolumeSlider.cs
+++ Assets/Scripts/VolumeSlider.cs
@@ -2,40 +2,40 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Reflection;
 using UnityEngine;
-using UnityEngine.UI;
+using UnityEngine.UI;//UI컴포넌트 사용하려면 써야하는 using문
 
-public class VolumeSlider : MonoBehaviour
+public class VolumeSlider : MonoBehaviour// 오디오 볼륨 조절 슬라이더 관리 클래스
 {
-    [SerializeField] Slider masterSlider;
-    [SerializeField] Slider fxSlider;
-    [SerializeField] Slider bgmSlider;
-
+    [SerializeField] Slider masterSlider;// 마스터 볼륨을 조절하는 슬라이더
+    [SerializeField] Slider fxSlider;// 효과음(SFX) 볼륨을 조절하는 슬라이더
+    [SerializeField] Slider bgmSlider; // 배경음악(BGM) 볼륨을 조절하는 슬라이더 (인스펙터에 직접 할당 가능)
 
     void Start()
     {
-        masterSlider.onValueChanged.AddListener(SetMasterVolume);
-        fxSlider.onValueChanged.AddListener(SetFxVolume);
-        bgmSlider.onValueChanged.AddListener(SetBgmVolume);
+        masterSlider.onValueChanged.AddListener(SetMasterVolume);// 마스터 슬라이더 값이 바뀔 때 SetMasterVolume 실행
+        fxSlider.onValueChanged.AddListener(SetFxVolume); // 효과음 슬라이더 값이 바뀔 때 SetFxVolume 실행
+        bgmSlider.onValueChanged.AddListener(SetBgmVolume);// 배경음악 슬라이더 값이 바뀔 때 SetBgmVolume 실행
 
-        masterSlider.value = 1f;
-        fxSlider.value = 0.5f;
-        bgmSlider.value = 0.5f;
+        masterSlider.value = 1f;// 마스터 볼륨 기본값을 1.0으로 설정 (최대)
+        fxSlider.value = 0.5f;// 효과음 볼륨 기본값을 0.5로 설정
+        bgmSlider.value = 0.5f;// 배경음악 볼륨 기본값을 0.5로 설정
     }
 
 
     public void SetMasterVolume(float volume)
     {
-        AudioListener.volume = volume;
+        AudioListener.volume = volume;// 전체 오디오(마스터 볼륨)를 조절
     }
 
     public void SetFxVolume(float volume)
     {
-        AudioManager.Instance.SetSfxVolume(volume);
+        AudioManager.Instance.SetSfxVolume(volume);// AudioManager를 통해 효과음 볼륨 조절
     }
+    
 
     public void SetBgmVolume(float volume)
     {
-        AudioManager.Instance.SetBgmVolume(volume);
+        AudioManager.Instance.SetBgmVolume(volume);// AudioManager를 통해 배경음악 볼륨 조절
     }
 
 }
